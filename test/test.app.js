72b18559caa74db8f432d0dfe6ac2b903d3ee24a
Oscar A. Reyes <oscarreyes2194@gmail.com>
2019-01-17 15:24:11 -0600

    set up travis

---
+++ test/test.app.js
@@ -0,0 +1,20 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const expect = chai.expect;
+
+const {app} = require('../server');
+
+chai.use(chaiHttp);
+
+describe('should add 200 with html display', function() {
+  
+  it('should be 200 status', function() {
+    return chai.request(app)
+    .get('/')
+    .then(function(res) {
+      expect(res).to.be.status(200);
+      //expect(res,body).to.deep.equal('<p>Hello world</p>');
+    }) 
+  })
+})
